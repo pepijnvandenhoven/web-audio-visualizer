fab4ffb599133a2702535a7d8895d3d1f5a4d6c5
pepvdh <pepvdh@gmail.com>
2019-09-18 22:43:36 +0200

    Doing things, getting somewhere

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,13 @@
+// TODO: Throttle requestAnimationFrame and add FPS count - https://stackoverflow.com/questions/19764018/controlling-fps-with-requestanimationframe
 import React, { Component, RefObject } from 'react';
 import "./App.scss";
 import PlayPause from '../PlayPause/PlayPause';
+import { Color, IColorBufferItem } from '../../helpers/Color';
 
-const FFT_SIZE = 2048;
+interface IColorRotateObject {
+	value: number;
+	up: boolean;
+}
 
 interface IProps {}
 
@@ -12,6 +17,14 @@ interface IState {
 }
 
 class App extends Component<IProps, IState> {
+	// Settings
+	private readonly FFT_SIZE = 128;
+	private readonly SAMPLE_RATE = 26000;
+	private readonly COLOR_SMOOTHING = 4;
+
+	// Helpers
+	Color = new Color();
+
 	audioRef: RefObject<HTMLAudioElement>;
 	canvasRef: RefObject<HTMLCanvasElement>;
 
@@ -21,8 +34,24 @@ class App extends Component<IProps, IState> {
 	audioDataArray?: Uint8Array;
 	audioStream?: MediaStream;
 	
-	canvasContext?: CanvasRenderingContext2D;
-	canvasElement?: HTMLCanvasElement;
+	canvasContext: CanvasRenderingContext2D | null = null;
+	canvasElement: HTMLCanvasElement | null = null;
+
+	readonly colorBufferLength = this.FFT_SIZE / 2;
+	readonly colorStep = 255 / this.colorBufferLength / this.COLOR_SMOOTHING;
+	colorBufferArray: Array<IColorBufferItem> = [];
+	rotateR: IColorRotateObject = {
+		value: 170,
+		up: false
+	};
+	rotateG: IColorRotateObject = {
+		value: 85,
+		up: false
+	};
+	rotateB: IColorRotateObject = {
+		value: 85,
+		up: true
+	};
 	
 	constructor(props: IProps) {
 		super(props);
@@ -35,37 +64,91 @@ class App extends Component<IProps, IState> {
 		this.audioRef = React.createRef();
 		this.canvasRef = React.createRef();
 
-		this.init = this.init.bind(this);
-		this.drawVisuals = this.drawVisuals.bind(this);
+		this.drawLoop = this.drawLoop.bind(this);
 		this.handleChangePlayPause = this.handleChangePlayPause.bind(this);
+		this.colorRotateLoop = this.colorRotateLoop.bind(this);
 	}
 
-	componentDidMount() {
-		if (this.state.isPlaying) {
-			this.init();
+	rotateColor(color: IColorRotateObject) {
+		// Mind requestAnimationFrame!
+		let step = this.colorStep;
+		if((color.value >= 255 && color.up) || (color.value <= 0 && !color.up)) {
+			color.up = !color.up;
+		}
+		color.value = color.up ? color.value+step : color.value-step;
+		return color;
+	}
+
+	colorRotateStep() {
+		// Mind requestAnimationFrame!
+		this.rotateR = this.rotateColor(this.rotateR);
+		this.rotateG = this.rotateColor(this.rotateG);
+		this.rotateB = this.rotateColor(this.rotateB);
+	}
+
+	colorRotateLoop() {
+		// Mind requestAnimationFrame!
+		let colorRotateFrame;
+
+		// Stop drawing, eg. on pause
+		if (!this.state.isPlaying) {
+			console.log('[colorRotateAnimate] Stop drawing')
+			if (colorRotateFrame) {
+				window.cancelAnimationFrame(colorRotateFrame);
+			}
+			return;
+		}
+		
+		// Keep looping the color rotate loop once it has been started
+		colorRotateFrame = requestAnimationFrame(this.colorRotateLoop);
+
+		// Assign a new color to each frequency range
+		let len = this.colorBufferLength-1;
+		for (let i = len; i > -1; i--) {
+			let prevColor: IColorBufferItem;
+			if (i > 0) {
+				prevColor = this.colorBufferArray[i-1];
+				this.rotateR.value = prevColor.r;
+				this.rotateG.value = prevColor.g;
+				this.rotateB.value = prevColor.b;
+			} else {
+				// Determine a new color of the first item
+				this.colorRotateStep();
+			}
+			this.colorBufferArray[i] = {
+				r: this.rotateR.value,
+				g: this.rotateG.value,
+				b: this.rotateB.value
+			};
 		}
 	}
 
-	public init() {
-		this.setupAudio().then(() => {
-			this.setupVisuals();
-			this.drawVisuals();
-		}, () => {
-			console.log('[init] Could not initiate automatically');
-		});
+	setupColorRotate() {
+		console.log('[setupColorRotate] called');
+		// Assign a color to each frequency range
+		let len = this.colorBufferLength-1;
+		for (let i = len; i > -1; i--) {
+			this.colorRotateStep();
+			this.colorBufferArray.unshift({
+				r: this.rotateR.value,
+				g: this.rotateG.value,
+				b: this.rotateB.value
+			});
+		}
+		this.colorRotateLoop();
 	}
 
-	public async setupAudio(): Promise<any> {
+	async setupAudio(): Promise<any> {
 		console.log('[setupAudio] called');
-		let audioAnalyser, 
-			audioContext,
-			audioStream,
-			audioSourceNode;
+		let audioSourceNode;
 		
 		// Create AudioContext
-		audioContext = new AudioContext();
+		this.audioContext = new AudioContext({
+			sampleRate: this.SAMPLE_RATE
+		});
 		
-		if (audioContext.state === 'suspended') {
+		// Check if we have permission to read audio
+		if (this.audioContext.state === 'suspended') {
 			this.setState({
 				initFailed: true,
 				isPlaying: false,
@@ -75,76 +158,75 @@ class App extends Component<IProps, IState> {
 		}
 
 		// Create audio analyser
-		audioAnalyser = audioContext.createAnalyser();
+		this.audioAnalyser = this.audioContext.createAnalyser();
+		this.audioAnalyser.minDecibels = -90;
+		this.audioAnalyser.maxDecibels = -30;
 
-		// Get the audio element
+		// Get the audio stream from user's microphone
 		try {
-			audioStream = await navigator.mediaDevices.getUserMedia({audio:true});
+			this.audioStream = await navigator.mediaDevices.getUserMedia({audio:true});
 		} catch(e) {
 			console.error("[setupAudio] failed");
 			throw(e);
 		}
 
-		// Pass the audio element into the audio context and connect the analyser
-		audioSourceNode = audioContext.createMediaStreamSource(audioStream);
-		audioSourceNode.connect(audioAnalyser);
-		// audioAnalyser.connect(audioContext.destination);
+		// Pass the audio stream into the audio context and connect the analyser
+		audioSourceNode = this.audioContext.createMediaStreamSource(this.audioStream);
+		audioSourceNode.connect(this.audioAnalyser);
+		
+		// Capture AnalyserNode data
+		this.audioAnalyser.fftSize = this.FFT_SIZE;
+		this.audioBufferLength = this.audioAnalyser.frequencyBinCount;
+		this.audioDataArray = new Uint8Array(this.audioBufferLength);
 
-		this.audioAnalyser = audioAnalyser;
-		this.audioStream = audioStream;
-		this.audioContext = audioContext;
 		return Promise.resolve();
 	}
 
-	public setupVisuals() {
-		console.log('[setupVisuals] called');
-		let audioAnalyser = this.audioAnalyser,
-			audioBufferLength,
-			audioDataArray,
-			canvasContext,
-			canvasElement = this.canvasRef.current;
-
-		canvasContext = canvasElement ? canvasElement.getContext("2d") : null;
-		// Check if all is present
-		if (!audioAnalyser || !canvasElement || !canvasContext) {
-			console.error("[setupVisuals] failed");
+	drawStep(index: number, xStart: number, yStart: number, xEnd: number, yEnd: number, a: number) {
+		// Mind requestAnimationFrame!
+		if (!this.canvasContext) {
+			console.error(`[drawRect] failed \n canvasContext: ${this.canvasContext}`);
 			return;
 		}
-		
-		// Capture AnalyserNode data
-		audioAnalyser.fftSize = FFT_SIZE;
-		audioBufferLength = audioAnalyser.frequencyBinCount;
-		audioDataArray = new Uint8Array(audioBufferLength);
 
-		// Store for later usage
-		this.audioBufferLength = audioBufferLength;
-		this.audioDataArray = audioDataArray;
-		this.canvasContext = canvasContext;
-		this.canvasElement = canvasElement;
+		let { r, g, b } = this.colorBufferArray[index],
+			gradient;
+
+		gradient = this.canvasContext.createLinearGradient(0, yStart, 0, yEnd);
+		gradient.addColorStop(0, 	`rgba(${r}, ${g}, ${b}, ${0})`);
+		gradient.addColorStop(0.8, 	`rgba(${r}, ${g}, ${b}, ${a})`);
+		gradient.addColorStop(1, 	`rgba(${r}, ${g}, ${b}, ${a/2})`);
+		this.canvasContext.fillStyle = gradient;
+		this.canvasContext.fillRect(xStart, yStart, xEnd, yEnd);
 	}
 
-	public drawVisuals() {
-		let audioAnalyser = this.audioAnalyser,
-			audioBufferLength = this.audioBufferLength,
-			audioDataArray = this.audioDataArray,
-			canvasContext = this.canvasContext,
-			canvasElement = this.canvasRef.current,
-			canvasHeight,
-			canvasWidth,
-			drawFrame,
-			posX,
-			posY,
-			sliceWidth;
+	drawLoop() {
+		// Mind requestAnimationFrame!
+		let drawFrame,
+			xStart = 0,
+			xEnd = 0,
+			yStart = 0,
+			yEnd = 0,
+			volume = 0,
+			averageVolume = 0,
+			gradient;
 
 		// Check if all is present
-		if (!audioAnalyser || !audioBufferLength || !audioDataArray || !canvasContext || !canvasElement) {
-			console.error(`[drawVisuals] failed \n audioAnalyser: ${audioAnalyser} \n audioBufferLength: ${audioBufferLength} \n audioDataArray: ${audioDataArray} \n canvasContext: ${canvasContext} \n canvasElement: ${canvasElement}`);
+		if (!this.audioAnalyser || !this.audioBufferLength || !this.audioDataArray || !this.canvasContext || !this.canvasElement) {
+			console.error(`
+				[drawVisuals] failed
+				audioAnalyser: ${this.audioAnalyser}
+				audioBufferLength: ${this.audioBufferLength}
+				audioDataArray: ${this.audioDataArray}
+				canvasContext: ${this.canvasContext}
+				canvasElement: ${this.canvasElement}
+			`);
 			return;
 		}
 
-		// Stop drawing
+		// Stop drawing, eg. on pause
 		if (!this.state.isPlaying) {
-			console.log('[drawVisuals] Stop drawing')
+			console.log('[drawVisuals] Stop drawing');
 			if (drawFrame) {
 				window.cancelAnimationFrame(drawFrame);
 			}
@@ -152,54 +234,58 @@ class App extends Component<IProps, IState> {
 		}
 
 		// Keep looping the drawing function once it has been started
-		drawFrame = requestAnimationFrame(this.drawVisuals);
+		drawFrame = requestAnimationFrame(this.drawLoop);
 
 		// Copy current frequency data into audioDataArray
-		audioAnalyser.getByteFrequencyData(audioDataArray);
-
-		// Store canvas width and height
-		canvasWidth = canvasElement.width;
-		canvasHeight = canvasElement.height;
+		this.audioAnalyser.getByteFrequencyData(this.audioDataArray);
 
 		// Clear canvas
-		canvasContext.clearRect(0, 0, canvasWidth, canvasHeight);
+		this.canvasContext.clearRect(0, 0, this.canvasElement.width, this.canvasElement.height);
 
-		// Make a line
-		canvasContext.lineWidth = 2;
-		let gradient = canvasContext.createLinearGradient(0, 0, canvasWidth, canvasHeight);
-		gradient.addColorStop(0, 'rgba(255, 0, 50, 1)');
-		gradient.addColorStop(1, 'rgba(50, 0, 255, 1)');
-		canvasContext.strokeStyle = gradient;
-		canvasContext.beginPath();
+		// Background
+		gradient = this.canvasContext.createLinearGradient(this.canvasElement.width, 0, 0, this.canvasElement.height);
+		gradient.addColorStop(1, this.Color.parse(this.Color.darken(this.colorBufferArray[0], 75)));
+		gradient.addColorStop(0, this.Color.parse(this.Color.darken(this.colorBufferArray[this.colorBufferLength-1], 95)));
+		this.canvasContext.fillStyle = gradient;
+		this.canvasContext.fillRect(0, 0, this.canvasElement.width, this.canvasElement.height);
 
 		// Determine the width of each segment of the line
-		sliceWidth = canvasWidth / audioBufferLength;
+		xEnd = Math.ceil(this.canvasElement.width / this.audioBufferLength);
 		
 		// Define position for each slice
-		posX = 0;
-		for(var i = 0; i < audioBufferLength; i++) {
-			posY = audioDataArray[i] / 255 * canvasHeight;
-			
-			// Turn it upside down
-			posY = canvasHeight - posY;
-			
-			// Move to the correct position
-			if(i === 0) {
-				canvasContext.moveTo(posX, posY);
-			} else {
-				canvasContext.lineTo(posX, posY);
-			}
+		xStart = 0;
+		averageVolume = this.audioDataArray[Math.round(this.FFT_SIZE / 8)];
+		for(var i = 0; i < this.audioBufferLength; i++) {
+			volume = this.audioDataArray[i]; // 0 - 255
+			yStart = this.canvasElement.height - volume / 255 * this.canvasElement.height;
+			yEnd = this.canvasElement.height;
+
+			// Draw rectangle
+			this.drawStep(
+				i,
+				xStart, yStart, xEnd, yEnd, 
+				(0.25 + (averageVolume / 255) / 4 * 3)
+			);
 
 			// Remember where to draw the next one
-			posX += sliceWidth;
+			xStart += xEnd;
 		}
-		canvasContext.lineTo(canvasWidth, canvasHeight);
-
-		// Draw the line
-		canvasContext.stroke();
 	}
 
-	public handleChangePlayPause() {
+	setupVisuals() {
+		console.log('[setupVisuals] called');
+		this.canvasElement = this.canvasRef.current;
+		this.canvasContext = this.canvasElement ? this.canvasElement.getContext("2d") : null;
+		
+		// Check if all is present
+		if (!this.canvasElement || !this.canvasContext) {
+			console.error("[setupVisuals] failed");
+			return;
+		}
+	}
+
+	handleChangePlayPause() {
+		console.log('[handleChangePlayPause] called');
 		if (this.state.initFailed) {
 			this.setState({
 				initFailed: false,
@@ -211,8 +297,26 @@ class App extends Component<IProps, IState> {
 			this.setState({
 				isPlaying: !this.state.isPlaying
 			}, () =>  { 
-				this.drawVisuals()
+				this.drawLoop();
+				this.colorRotateLoop();
 			});
+		}
+	}
+
+	init() {
+		console.log('[init] called');
+		this.setupColorRotate();
+		this.setupAudio().then(() => {
+			this.setupVisuals();
+			this.drawLoop();
+		}, () => {
+			console.log('[init] Could not initiate automatically');
+		});
+	}
+
+	componentDidMount() {
+		if (this.state.isPlaying) {
+			this.init();
 		}
 	}
 
--- src/components/PlayPause/PlayPause.tsx
+++ src/components/PlayPause/PlayPause.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlay, faPause } from '@fortawesome/free-solid-svg-icons';
+import "./PlayPause.scss";
 
 interface IProps {
 	isPlaying: boolean;
@@ -21,8 +22,8 @@ class PlayPause extends Component<IProps> {
 
 	render() {
 		return (
-			<div>
-				<button type="button" className="btn btn-primary" onClick={this.onPlayPause}>
+			<div className="PlayPause">
+				<button type="button" className="btn btn-secondary" onClick={this.onPlayPause}>
 					<FontAwesomeIcon icon={this.props.isPlaying ? faPause : faPlay} />
 				</button>
 			</div>
