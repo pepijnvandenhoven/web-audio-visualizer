30e98e2bf0f74bd9674c445ae9ed83076b25d415
pepvdh <pepvdh@gmail.com>
2020-04-04 21:55:59 +0200

    add new BOILERPLATE

---
+++ rollup.config.js
@@ -0,0 +1,15 @@
+import rimraf from "rimraf";
+import typescript from "@rollup/plugin-typescript";
+
+rimraf("dist", (err) => {
+	err && console.error(err);
+});
+
+export default {
+	input: "src/scripts/app.ts",
+	output: {
+		file: "dist/scripts/app.js",
+		format: "cjs"
+	},
+	plugins: [typescript()]
+};
