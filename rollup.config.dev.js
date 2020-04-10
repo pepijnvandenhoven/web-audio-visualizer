048f6c95339037bdfb75161f15e318de91de7f54
pepvdh <pepvdh@gmail.com>
2020-04-10 12:33:11 +0200

    add prod build and improve npm scripts

--- rollup.config.dev.js
+++ rollup.config.dev.js
@@ -1,25 +1,24 @@
 import typescript from "@rollup/plugin-typescript";
 import resolve from '@rollup/plugin-node-resolve';
-import { terser } from 'rollup-plugin-terser';
 import copy from 'rollup-plugin-copy';
 
 export default {
 	input: "src/scripts/app.ts",
 	output: [{
-		file: 'dist/scripts/bundle.min.js',
-		format: 'iife',
-		plugins: [
-			terser()
-		]
-    }],
+		file: "dist/scripts/bundle.js",
+		format: "cjs"
+	}],
 	plugins: [
 		typescript(),
 		resolve(),
 		copy({
-			targets: [{ 
-				src: 'src/index.html', 
-				dest: 'dist' 
-			}]
+			targets: [
+				{ 
+					src: 'src/index.html', 
+					dest: 'dist', 
+					transform: (contents) => contents.toString().replace(/.min./g, '.')
+			 	}
+			]
 		})
 	]
 };
